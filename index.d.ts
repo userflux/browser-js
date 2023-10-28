e732431b54521808459de06402347809b2a45654
luke <luke@userflux.co>
2023-10-28 15:35:04 +1100

    Update `index.d.ts` specs

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 // index.d.ts
-declare module '@userflux/analytics-js' {
-    export default class UserFlux {
+declare module '@userflux/browser-js' {
+    class UserFlux {
         static initialize(apiKey: string, options: object): void;
         static identify(attributes: object, userId?: string | undefined): void;
         static track(name: string, properties: object, userId?: string | undefined): void;
         static reset(): void;
     }
+    export { UserFlux as default, UserFlux };
 }
