9bb11195202b760629378d424a6fd7751d87ad41
luke <luke@userflux.co>
2023-10-31 16:04:07 +1100

    Update anon ID generator method

--- index.js
+++ index.js
@@ -56,10 +56,18 @@ class UserFlux {
     static getOrCreateAnonymousId() {
         let anonymousId = UserFlux.storage?.getItem('uf-anonymousId');
         if (!anonymousId) {
-            anonymousId = crypto.randomUUID();
+            anonymousId = UserFlux.generateUUID();
             UserFlux.storage?.setItem('uf-anonymousId', anonymousId);
         }
         return anonymousId;
+    }
+
+    static generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (Math.random() * 16) | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 
     static getUserId() {
